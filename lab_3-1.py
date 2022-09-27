6c0668e8fe64ec71522606fab38d51636ff766e2
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-09-27 10:48:16 -0400

    Completed labs 1 and 2

---
+++ lab_3-1.py
@@ -0,0 +1,9 @@
+# author: JHR 09/27/22
+
+magic_charge = 1
+shield_charge = 1
+
+if (magic_charge >= 90 and shield_charge >= 75):
+    print ("You defeated the Dragon! But the Princess is in another castle.")
+else:
+    print ("The Dragon burns you to a crisp.")
---
+++ lab_3-2.py
@@ -0,0 +1,9 @@
+# author: JHR 09/27/22
+
+# code - GuessError, ActualError
+
+# int (a) - Syntax Error, NameError
+# int ('a') - Value Error, ValueError
+# 'a' + 2 - TypeError, TypeError
+# import date - ModuleNotFoundError, ModuleNotFoundError
+# print ("I'm a happy camper!) - Syntax Error, Syntax Error
