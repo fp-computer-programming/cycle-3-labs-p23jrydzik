d9f749a632a85ddc7013cc5b1f047da2b6f4ea62
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-09-28 10:50:41 -0400

    finish 3.3

---
+++ lab_3-3.py
@@ -0,0 +1,15 @@
+# author: JHR 09/28/22
+
+import random
+
+x = random.randint(1,100)
+print (x)
+# My result was 58
+
+y = random.seed (94)
+print (random.random())
+# My result was 0.5468015535366331
+
+z = random.randrange(0,100,2) 
+print (z)
+# My result was 14
