1d20cc52e954bec98a127f5ccab58a7483c5dacf
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-09-29 12:05:35 -0400

    finished lab 3-4

---
+++ lab_3-4.py
@@ -0,0 +1,47 @@
+# author: JHR 09/29/22
+
+import math
+
+# 1. 8
+# 2. 19
+# 3. 42
+# 4. 8
+# 5. 2
+# 6. 3
+# 7. 1
+# 8. 120
+# 9. 333
+# 10. 0
+
+a = math.ceil(7.15)
+b = math.floor(19.156)
+c = math.fabs(-42)
+d = math.pow(2, 3)
+e = math.log10(100)
+f = math.sqrt(9)
+g = math.isqrt(2)
+h = math.factorial(5)
+i = math.gcd(111111111, 12321)
+j = math.sin(0)
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+print (g)
+print (h)
+print (i)
+print (j)
+
+# 1. 8
+# 2. 19
+# 3. 42.0
+# 4. 8.0
+# 5. 2.0
+# 6. 3.0
+# 7. 1
+# 8. 120
+# 9. 333
+# 10. 0.0
