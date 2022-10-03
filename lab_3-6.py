f8400f9172719390a374f991fa8dab4217b9c949
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-10-03 14:28:16 -0400

    finished lab 3-6

---
+++ lab_3-6.py
@@ -0,0 +1,17 @@
+# author: JHR 10/03/22
+
+var1 = "Fairfield"
+var2 = "Prep"
+var3 = var1 + " " + var2
+print (var3)
+
+var4 = "-"
+var5 = var4 * 20
+print (var5)
+
+var6 = "I\'m loving this short story I\'m reading, \"The Fall of the House of Usher"
+print (len(var6))
+
+var7 = "apple"
+var8 = var7[3]
+print (var8)
